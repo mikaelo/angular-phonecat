ee788f638690277c396138bd80c812e5f2b21e3c
Mikhail Ushanov <mikaelo@indx.io>
2013-08-26 00:56:52 +0400

    Change config/* files to confirm last version of karma

--- config/karma-e2e.conf.js
+++ config/karma-e2e.conf.js
@@ -1,22 +1,21 @@
-basePath = '../';
+module.exports = function(config) {
 
-files = [
-  ANGULAR_SCENARIO,
-  ANGULAR_SCENARIO_ADAPTER,
-  'test/e2e/**/*.js'
-];
-
-autoWatch = false;
-
-browsers = ['Chrome'];
-
-singleRun = true;
-
-proxies = {
-  '/': 'http://localhost:8000/'
-};
-
-junitReporter = {
-  outputFile: 'test_out/e2e.xml',
-  suite: 'e2e'
+	config.set({
+		basePath : '../',
+		frameworks : ["ng-scenario"],
+		files : [
+  			'test/e2e/**/*.js'
+		],
+		autoWatch : false,
+		browsers : ['Chrome'],
+		singleRun : true,
+		proxies : {
+  			'/': 'http://localhost:8000/'
+		},
+		urlRoot: '/_karma_/',		
+		junitReporter : {
+		  outputFile: 'test_out/e2e.xml',
+		  suite: 'e2e'
+		}
+	});
 };
--- config/karma.conf.js
+++ config/karma.conf.js
@@ -1,20 +1,20 @@
-basePath = '../';
+module.exports = function(config) {
 
-files = [
-  JASMINE,
-  JASMINE_ADAPTER,
-  'app/lib/angular/angular.js',
-  'app/lib/angular/angular-*.js',
-  'test/lib/angular/angular-mocks.js',
-  'app/js/**/*.js',
-  'test/unit/**/*.js'
-];
-
-autoWatch = true;
-
-browsers = ['Chrome'];
-
-junitReporter = {
-  outputFile: 'test_out/unit.xml',
-  suite: 'unit'
+	config.set({
+		basePath : '../',
+		frameworks : ["jasmine"],
+		files : [
+			'app/lib/angular/angular.js',
+			'app/lib/angular/angular-*.js',
+			'test/lib/angular/angular-mocks.js',
+			'app/js/**/*.js',
+			'test/unit/**/*.js'
+		],
+		autoWatch : true,
+		browsers : ['Chrome'],
+		junitReporter : {
+			outputFile: 'test_out/unit.xml',
+			suite: 'unit'
+		}
+	});
 };
